14cac4bded44e3fd5cd6a9c3ba5585b731fb0f87
SODADAM <apinan403@gmail.com>
2022-05-03 09:23:58 +0700

    resize image

--- app/src/main/java/com/customcamerax/CameraActivity.kt
+++ app/src/main/java/com/customcamerax/CameraActivity.kt
@@ -1,49 +1,50 @@
 package com.customcamerax
 
 import android.Manifest
-import android.app.Activity
-import android.app.Instrumentation
-import android.content.ContentResolver
 import android.content.ContentValues
-import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
-import android.graphics.Bitmap
-import android.graphics.BitmapFactory
-import android.net.Uri
 import android.os.Build
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Environment
 import android.provider.MediaStore
 import android.util.Log
-import android.widget.Toast
-import androidx.activity.result.ActivityResult
-import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
 import androidx.camera.core.*
 import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.core.app.ActivityCompat
-import androidx.core.content.ContentProviderCompat.requireContext
 import androidx.core.content.ContextCompat
 import com.customcamerax.databinding.ActivityCameraBinding
-import java.io.*
+import com.customcamerax.utilities.stringResToast
 import java.nio.ByteBuffer
 import java.text.SimpleDateFormat
 import java.util.*
 import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
-import android.hardware.Camera.PictureCallback
+
+typealias LumaListener = (luma: Double) -> Unit
 
 class CameraActivity : AppCompatActivity() {
-    lateinit var cameraBinding: ActivityCameraBinding
+    private lateinit var viewBindingCameraActivity: ActivityCameraBinding
     private lateinit var cameraExecutor: ExecutorService
     private var imageCapture: ImageCapture? = null
+    private var imageAnalyzer: ImageAnalysis? = null
+
+    companion object {
+        const val TAG = "CameraXApp"
+        const val FILENAME_FORMAT = "yyyyMMddHHmmss"
+        private const val REQUEST_CODE_PERMISSIONS = 10
+        private val REQUIRED_PERMISSIONS = mutableListOf (Manifest.permission.CAMERA).apply {
+            if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.P) {
+                add(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+            }
+        }.toTypedArray()
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         supportActionBar!!.hide()
-        cameraBinding = ActivityCameraBinding.inflate(layoutInflater)
-        setContentView(cameraBinding.root)
+        viewBindingCameraActivity = ActivityCameraBinding.inflate(layoutInflater)
+        setContentView(viewBindingCameraActivity.root)
 
         if (allPermissionsGranted()) {
             startCamera()
@@ -53,10 +54,11 @@ class CameraActivity : AppCompatActivity() {
             )
         }
 
-        cameraBinding.activityCameraBtnImageCapture.setOnClickListener{ takePhoto()}
-        cameraBinding.activityCameraBtnBack.setOnClickListener {
+        viewBindingCameraActivity.activityCameraBtnImageCapture.setOnClickListener{ takePhoto()}
+        viewBindingCameraActivity.activityCameraBtnBack.setOnClickListener {
             startActivity(Intent(this, MainActivity::class.java))
         }
+
         cameraExecutor = Executors.newSingleThreadExecutor()
     }
 
@@ -68,9 +70,8 @@ class CameraActivity : AppCompatActivity() {
             if (allPermissionsGranted()) {
                 startCamera()
             } else {
-                Toast.makeText(this,
-                    "Permissions not granted by the user.",
-                    Toast.LENGTH_SHORT).show()
+                stringResToast(message = "Permissions not granted by the user.")
+//                Toast.makeText(this, "Permissions not granted by the user.", Toast.LENGTH_SHORT).show()
                 finish()
             }
         }
@@ -80,58 +81,6 @@ class CameraActivity : AppCompatActivity() {
         super.onDestroy()
         cameraExecutor.shutdown()
     }
-
-    private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
-        ContextCompat.checkSelfPermission(
-            baseContext, it) == PackageManager.PERMISSION_GRANTED
-    }
-
-
-    private fun takePhoto() {
-        // Get a stable reference of the modifiable image capture use case
-        val imageCapture = imageCapture ?: return
-
-        // Create time stamped name and MediaStore entry.
-        val name = SimpleDateFormat(FILENAME_FORMAT, Locale.US)
-            .format(System.currentTimeMillis())
-        val contentValues = ContentValues().apply {
-            put(MediaStore.MediaColumns.DISPLAY_NAME, name)
-            put(MediaStore.MediaColumns.MIME_TYPE, "image/jpeg")
-            if(Build.VERSION.SDK_INT > Build.VERSION_CODES.P) {
-                put(MediaStore.Images.Media.RELATIVE_PATH, "Pictures/CustomCameraX")
-            }
-        }
-
-        // Create output options object which contains file + metadata
-        val outputOptions = ImageCapture.OutputFileOptions
-            .Builder(contentResolver,
-                MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
-                contentValues)
-            .build()
-        // Set up image capture listener, which is triggered after photo has
-        // been taken
-        imageCapture.takePicture(
-            outputOptions,
-            ContextCompat.getMainExecutor(this),
-            object : ImageCapture.OnImageSavedCallback {
-                override fun onError(exc: ImageCaptureException) {
-                    Log.e(TAG, "Photo capture failed: ${exc.message}", exc)
-                }
-
-                override fun onImageSaved(output: ImageCapture.OutputFileResults){
-                    val msg = "Photo capture succeeded: ${output.getSavedUri()}"
-                    Toast.makeText(baseContext, msg, Toast.LENGTH_SHORT).show()
-                    Log.d(TAG, msg)
-                    setResult(RESULT_OK, Intent().putExtra("picturePath", output.getSavedUri().toString()))
-                    finish()
-                }
-            }
-        )
-//        val intent = Intent()
-//        intent.putExtra("picturePath", contentValues)
-//        setResult(RESULT_OK, intent)
-    }
-
 
     private fun startCamera() {
         val cameraProviderFuture = ProcessCameraProvider.getInstance(this)
@@ -144,14 +93,25 @@ class CameraActivity : AppCompatActivity() {
             val preview = Preview.Builder()
                 .build()
                 .also {
-                    it.setSurfaceProvider(cameraBinding.viewFinder.surfaceProvider)
+                    it.setSurfaceProvider(viewBindingCameraActivity.viewFinder.surfaceProvider)
                 }
 
             imageCapture = ImageCapture.Builder()
                 .build()
 
+            imageAnalyzer = ImageAnalysis.Builder()
+                .build()
+                .also {
+                    it.setAnalyzer(cameraExecutor, LuminosityAnalyzer {
+//                            luma -> Log.d(TAG, "Average luminosity: $luma")
+                    })
+                }
+
             // Select back camera as a default
-            val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+            val cameraSelector = CameraSelector.Builder()
+                .requireLensFacing(CameraSelector.LENS_FACING_BACK)
+                .build()
+//            val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
 
             try {
                 // Unbind use cases before rebinding
@@ -159,69 +119,88 @@ class CameraActivity : AppCompatActivity() {
 
                 // Bind use cases to camera
                 cameraProvider.bindToLifecycle(
-                    this, cameraSelector, preview, imageCapture)
+                    this, cameraSelector, preview, imageCapture, imageAnalyzer)
 
             } catch(exc: Exception) {
                 Log.e(TAG, "Use case binding failed", exc)
             }
-
         }, ContextCompat.getMainExecutor(this))
 
     }
 
-    companion object {
-        private val TAG = "CameraXApp"
-        private val FILENAME_FORMAT = "yyyyMMddHHmmss"
-        private const val REQUEST_CODE_PERMISSIONS = 10
-        private val REQUIRED_PERMISSIONS =
-            mutableListOf (
-                Manifest.permission.CAMERA
-            ).apply {
-                if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.P) {
-                    add(Manifest.permission.WRITE_EXTERNAL_STORAGE)
-                }
-            }.toTypedArray()
-    }
-}
+    private fun takePhoto() {
+        // Get a stable reference of the modifiable image capture use case
+        val imageCapture = imageCapture ?: return
 
-/*    private fun imageProxyToBitmap(image: ImageProxy): Bitmap {
-        val planeProxy = image.planes[0]
-        val buffer: ByteBuffer = planeProxy.buffer
-        val bytes = ByteArray(buffer.remaining())
-        buffer.get(bytes)
-        return BitmapFactory.decodeByteArray(bytes, 0, bytes.size)
-    }*/
+        // Create time stamped name and MediaStore entry.
+        val fName = SimpleDateFormat(FILENAME_FORMAT, Locale.US)
+            .format(System.currentTimeMillis())
 
-/*        val outputOptions = ImageCapture.OutputFileOptions
-            .Builder(contentResolver,
-                MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
-                contentValues)
-            .build()*/
+        // save to folder Android/Media
+//        val imageFile = File(outputDirectory, "$fName.jpg")
 
-/*        imageCapture.takePicture(
+        val contentValues = ContentValues().apply {
+            put(MediaStore.MediaColumns.DISPLAY_NAME, fName)
+            put(MediaStore.MediaColumns.MIME_TYPE, "image/jpeg")
+            if(Build.VERSION.SDK_INT > Build.VERSION_CODES.P) {
+                put(MediaStore.Images.Media.RELATIVE_PATH, "Pictures/${resources.getString(R.string.app_name)}")
+            }
+        }
+
+        // Create output options object which contains file + metadata
+/*        val outputOptions = this.contentResolver.let {
+            ImageCapture.OutputFileOptions.Builder(imageFile).build()
+        }*/
+            // for ContentValues
+        val outputOptions = ImageCapture.OutputFileOptions
+            .Builder(contentResolver, MediaStore.Images.Media.EXTERNAL_CONTENT_URI, contentValues)
+            .build()
+
+        // Set up image capture listener, which is triggered after photo has
+        // been taken
+        imageCapture.takePicture(
+            outputOptions,
             ContextCompat.getMainExecutor(this),
-            object : ImageCapture.OnImageCapturedCallback() {
-                override fun onCaptureSuccess(image: ImageProxy) {
-
-                    val bitmap: Bitmap = imageProxyToBitmap(image)
-
-                    val stream = ByteArrayOutputStream()
-                    bitmap.compress(Bitmap.CompressFormat.PNG, 100, stream)
-                    val byteArray = stream.toByteArray()
-                    bitmap.recycle()
-//                    val stream = ByteArrayOutputStream()
-//                    bitmap.compress(Bitmap.CompressFormat.PNG, 100, stream)
-//                    val byteArray: ByteArray = stream.toByteArray()
-                    Log.d(TAG, image.toString())
-                    setResult(RESULT_OK, Intent().putExtra("picturePath", byteArray))
-                    finish()
-                    super.onCaptureSuccess(image)
-                }
-
+            object : ImageCapture.OnImageSavedCallback {
                 override fun onError(exc: ImageCaptureException) {
                     Log.e(TAG, "Photo capture failed: ${exc.message}", exc)
-                    super.onError(exc)
+                }
+
+                override fun onImageSaved(output: ImageCapture.OutputFileResults){
+                    val msg = "Photo capture succeeded: ${output.savedUri}"
+                    stringResToast(message = msg)
+//                    Toast.makeText(baseContext, msg, Toast.LENGTH_SHORT).show()
+                    Log.d(TAG, msg)
+                    setResult(RESULT_OK, Intent().putExtra("picturePath", output.savedUri.toString()))
+                    finish()
                 }
             }
-        )*/
+        )
+    }
 
+    private class LuminosityAnalyzer(private val listener: LumaListener) : ImageAnalysis.Analyzer {
+
+        private fun ByteBuffer.toByteArray(): ByteArray {
+            rewind()    // Rewind the buffer to zero
+            val data = ByteArray(remaining())
+            get(data)   // Copy the buffer into a byte array
+            return data // Return the byte array
+        }
+
+        override fun analyze(image: ImageProxy) {
+
+            val buffer = image.planes[0].buffer
+            val data = buffer.toByteArray()
+            val pixels = data.map { it.toInt() and 0xFF }
+            val luma = pixels.average()
+
+            listener(luma)
+
+            image.close()
+        }
+    }
+
+    private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
+        ContextCompat.checkSelfPermission(baseContext, it) == PackageManager.PERMISSION_GRANTED
+    }
+}
--- app/src/main/java/com/customcamerax/MainActivity.kt
+++ app/src/main/java/com/customcamerax/MainActivity.kt
@@ -1,78 +1,207 @@
 package com.customcamerax
 
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
-import com.customcamerax.databinding.ActivityMainBinding
-import androidx.core.app.ActivityCompat.startActivityForResult
-import android.R
-import android.app.Activity
+import android.content.ContentResolver
 import android.content.Context
+import android.content.Intent
 import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.graphics.ImageDecoder
 import android.net.Uri
+import android.os.Build
+import android.os.Bundle
+import android.os.Environment
+import android.os.Environment.DIRECTORY_PICTURES
 import android.provider.MediaStore
 import android.util.Log
-import android.view.View
-
-import android.widget.TextView
-import android.widget.Toast
-import java.io.ByteArrayOutputStream
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.net.toUri
+import androidx.core.view.isVisible
+import androidx.lifecycle.lifecycleScope
+import com.canhub.cropper.CropImageContract
+import com.canhub.cropper.CropImageView
+import com.canhub.cropper.options
+import com.customcamerax.databinding.ActivityMainBinding
+import com.customcamerax.utilities.ImageUtilities
+import com.customcamerax.utilities.stringResToast
+import id.zelory.compressor.Compressor
+import id.zelory.compressor.constraint.destination
+import id.zelory.compressor.constraint.quality
+import kotlinx.coroutines.launch
+import java.io.File
+import java.io.FileOutputStream
+import java.io.IOException
+import java.io.OutputStream
 import java.text.SimpleDateFormat
 import java.util.*
-import android.graphics.BitmapFactory
-import androidx.camera.core.ImageProxy
-import java.nio.ByteBuffer
 
-class MainActivity : AppCompatActivity() {
-    lateinit var bindingmainActivity: ActivityMainBinding
-    private val CAMERA_ACTIVITY_REQUEST_CODE = 0
+
+class MainActivity : AppCompatActivity(){
+    private lateinit var viewBindingMainActivity: ActivityMainBinding
+    private val imageUtilities = ImageUtilities()
+
+    private val startIntentActivityCamera = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()) {result ->
+        if (result.resultCode == RESULT_OK) {
+            val imageUri = Uri.parse(result.data?.getStringExtra("picturePath"))
+            Log.d(CameraActivity.TAG, "Uri: $imageUri")
+
+            val uri = Uri.parse(getRealPath(this, imageUri))
+            val file = File(uri.path.toString())
+            Log.d(CameraActivity.TAG, "Picture Path: ${uri.path.toString()}")
+
+/*            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+                resizeImage(ImageDecoder.decodeBitmap(ImageDecoder.createSource(applicationContext.contentResolver, imageUri)))
+            } else {
+                resizeImage(MediaStore.Images.Media.getBitmap(applicationContext.contentResolver, imageUri))
+            }*/
+
+            viewBindingMainActivity.imageViewShowImage.setImageURI(imageUri)
+            viewBindingMainActivity.buttonCapture.isVisible = false
+
+            // Normal ขนาดไฟล์เล็กกว่า คุณภาพของรูปด้อยกว่านิดหน่อย dimention ไม่เหมือนเดิม
+            resizeImage(imageUtilities.viewToBitmap(viewBindingMainActivity.imageViewShowImage))
+
+            // Use library ขนาดไฟล์ใหญ่กว่า คุณภาพของรูปดีกว่านิดหน่อย dimention เหมือนเดิม
+//            compressImage(FileUtil.from(this, imageUri))
+
+            cropImage(imageUri)
+            file.deleteRecursively()
+            Log.d(CameraActivity.TAG, "delete file " + file.path)
+        }
+    }
+
+    private val cropImageActivityResult = registerForActivityResult(CropImageContract()) { result ->
+        if (result.isSuccessful) {
+            val imageUri: Uri? = result.uriContent
+
+            viewBindingMainActivity.imageViewShowImage.setImageURI(imageUri)
+            imageUtilities.saveImage(viewBindingMainActivity.imageViewShowImage, this, "Crop")
+        } else {
+            val exception = result.error
+            Log.e(CameraActivity.TAG, "Exception: $exception")
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        bindingmainActivity = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(bindingmainActivity.root)
+        viewBindingMainActivity = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(viewBindingMainActivity.root)
 
-        bindingmainActivity.activityMainIVShowImage.setOnClickListener {
-            val intent = Intent(this, CameraActivity::class.java)
-            startActivityForResult(intent, CAMERA_ACTIVITY_REQUEST_CODE)
+        viewBindingMainActivity.buttonCapture.setOnClickListener {
+            val openCameraActivity = Intent(this, CameraActivity::class.java)
+            startIntentActivityCamera.launch(openCameraActivity)
+        }
+
+        viewBindingMainActivity.imageViewShowImage.setOnClickListener {
+            val openCameraActivity = Intent(this, CameraActivity::class.java)
+            startIntentActivityCamera.launch(openCameraActivity)
         }
     }
 
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        // check that it is the SecondActivity with an OK result
-        if (resultCode == RESULT_OK && requestCode == CAMERA_ACTIVITY_REQUEST_CODE) {
-            val uri = Uri.parse(data?.getStringExtra("picturePath"))
-            Log.d("CameraXApp", "Uri: $uri")
-            bindingmainActivity.activityMainIVShowImage.setImageURI(uri)
+    private fun resizeImage(bitmap: Bitmap) {
+        viewBindingMainActivity.buttonResizeImg.isVisible = true
+        viewBindingMainActivity.buttonResizeImg.setOnClickListener {
+            // resize with reduce dimention
+            val size = 1250
+            viewBindingMainActivity.imageViewShowImage.setImageBitmap(imageUtilities.getResizedBitmap(bitmap, size))
+            imageUtilities.saveImage(imageUtilities.getResizedBitmap(bitmap, size), this, "Resize")
+
+            // resize with reduce quality
+/*            viewBindingMainActivity.imageViewShowImage.setImageBitmap(bitmap)
+            imageUtilities.saveImage(viewBindingMainActivity.imageViewShowImage, this, "Resize")*/
+//            val resizedImage = Bitmap.createScaledBitmap(bitmap, 500, 600, true)
         }
+    }
+
+    private fun cropImage(uri: Uri) {
+        viewBindingMainActivity.buttonCropImg.isVisible = true
+        viewBindingMainActivity.buttonCropImg.setOnClickListener{
+            cropImageActivityResult.launch(
+                options(uri) {
+                    setGuidelines(CropImageView.Guidelines.ON)
+                    setAspectRatio(1920, 1080)
+                    setCropShape(CropImageView.CropShape.RECTANGLE)
+//                    setOutputCompressFormat(Bitmap.CompressFormat.PNG)
+                }
+            )
+        }
+    }
+
+    private fun compressImage(file: File) {
+        viewBindingMainActivity.buttonResizeImg.isVisible = true
+        viewBindingMainActivity.buttonResizeImg.setOnClickListener {
+            file.let {
+                lifecycleScope.launch {
+                    val compressedImageFile: File = Compressor.compress(this@MainActivity, it) {
+                        val fileName = "resize_${SimpleDateFormat(CameraActivity.FILENAME_FORMAT, Locale.US)
+                                .format(System.currentTimeMillis())}.jpg"
+
+                        val imageFile = File("${Environment.getExternalStoragePublicDirectory(DIRECTORY_PICTURES)}" +
+                                "/${this@MainActivity.resources.getString(R.string.app_name)}/$fileName")
+
+                        quality(20) //ไฟล์รูปที่ได้จะชัดกว่า แต่ถ้าไฟล์เดิมขนาดใหญ่มาก (6|7 MB+) ขนาดไฟล์น่าจะลดลงไปเหลือประมาณ 1MB++
+//                        size(1_048_576) //limit less size 1 MB // ยิ่งขนาดเล็ก noise ยิ่งเห็นชัด เพราะการทำงานเบื้องหลังเป็นการลด quality
+                        destination(imageFile)
+                    }
+                    stringResToast(message = "Compressed image save in ${it.path}")
+                    Log.d(CameraActivity.TAG, "Compressed image save in ${it.path}")
+                    setCompressedImage(it)
+
+                    val updateGallery = Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE)
+                    updateGallery.data = compressedImageFile.toUri()
+                    this@MainActivity.sendBroadcast(updateGallery)
+                }
+            }
+        }
+    }
+
+    private fun setCompressedImage(file: File) {
+        file.let {
+            viewBindingMainActivity.imageViewShowImage.setImageBitmap(BitmapFactory.decodeFile(it.absolutePath))
+
+            if (it.exists()) {
+                if (it.deleteRecursively()) {
+                    Log.d(CameraActivity.TAG, "delete file " + it.path)
+                }
+            }
+//            val lengthFile: Long = it.length()/1024
+//            Log.d(CameraActivity.TAG,  "ขนาดไฟล์ $lengthFile KB")
+//            imageUtilities.saveImage(viewBindingMainActivity.imageViewShowImage, this, "Resize")
+        }
+    }
+
+    private fun getRealPath(context: Context, uri: Uri): String? {
+        val contentResolver: ContentResolver = context.contentResolver ?: return null
+
+        // Create file path inside app's data dir
+        val filePath: String = (context.applicationInfo.dataDir.toString() + File.separator
+                + System.currentTimeMillis())
+        val file = File(filePath)
+        try {
+            val inputStream = contentResolver.openInputStream(uri) ?: return null
+            val outputStream: OutputStream = FileOutputStream(file)
+            val buf = ByteArray(1024)
+            var len: Int
+            while (inputStream.read(buf).also { len = it } > 0) outputStream.write(buf, 0, len)
+            outputStream.close()
+            inputStream.close()
+        } catch (ignore: IOException) {
+            return null
+        }
+        return file.absolutePath
     }
 }
 
-//            val byteArray = intent.getByteArrayExtra("picturePath")
-//            var bitmap = byteArray?.size?.let { BitmapFactory.decodeByteArray(byteArray, 0, it) }
-//            val bitmap = data?.getParcelableExtra<ByteArray>("picturePath")
-//            val uri: Uri? = getImageUri(applicationContext, bitmap) // GET URI FROM THE BITMAP
-//            Toast.makeText(baseContext, "Get Image From $uri", Toast.LENGTH_SHORT).show()
-//            val imageProxy = data?.getStringExtra("picturePath")
-//            val bitmap: Bitmap = imageProxyToBitmap(imageProxy)
+/*            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+                val source: ImageDecoder.Source = ImageDecoder.createSource(applicationContext.contentResolver, imageUri)
+                val bitmap = ImageDecoder.decodeBitmap(source)
+                resizeImage(bitmap)
+            } else {
+                val bitmap = MediaStore.Images.Media.getBitmap(applicationContext.contentResolver, imageUri)
+                resizeImage(bitmap)
+            }*/
 
 /*
-private fun getImageUri(inContext: Context, inImage: Bitmap): Uri? {
-    val bytes = ByteArrayOutputStream()
-    inImage.compress(Bitmap.CompressFormat.JPEG, 100, bytes)
-    val path = MediaStore.Images.Media.insertImage(inContext.getContentResolver(),
-        inImage,
-        "IMG"+ SimpleDateFormat(
-            "yyyyMMddHHmm", Locale.US).format(System.currentTimeMillis()),
-        null)
-    return Uri.parse(path)
-}
-
-private fun imageProxyToBitmap(image: ImageProxy): Bitmap {
-    val planeProxy = image.planes[0]
-    val buffer: ByteBuffer = planeProxy.buffer
-    val bytes = ByteArray(buffer.remaining())
-    buffer.get(bytes)
-    return BitmapFactory.decodeByteArray(bytes, 0, bytes.size)
-}*/
+val lengthFile: Long = file.length()/1024
+Log.d("CameraXApp",  "ขนาดไฟล์ ${ lengthFile } KB")*/
